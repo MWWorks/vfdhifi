6be2c8c4f2be834d3f603065696180cf2c79364f
MWWorks <mw@robotmouse.com>
2022-03-02 09:27:30 +0000

    Add files via upload

--- hifimpd.py
+++ hifimpd.py
@@ -1,8 +1,3 @@
-# i2ctest.py
-# A brief demonstration of the Raspberry Pi I2C interface, using the Sparkfun
-# Pi Wedge breakout board and a SparkFun MCP4725 breakout board:
-# https://www.sparkfun.com/products/8736
-
 import sys
 import time
 import signal
